56ea8047b2bc63573927ec8807d5446678d8484d
AM <arshadm.2406@gmail.com>
2023-07-10 12:04:36 +0530

    2nd commit

--- hello.ts
+++ hello.ts
@@ -1,5 +1,17 @@
 let user  = "abc";
+type rgb = [number,number,number];
 
+let rgbval: rgb = [124,232,255];
+
+type unionType= string|boolean;
+
+let unionTypeExample: unionType = true;
+unionTypeExample = "not true";
+
+type obj1= {a:string};
+type obj2 = {b:number};
+type inter = obj1 & obj2;
+let egObj1:inter = {a:"21",b:21}
 console.log(3+2);
 console.log(user);
 
