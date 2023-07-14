4ac471df504eb8c47119c5fd940ba32106bd461b
AM <arshadm.2406@gmail.com>
2023-07-14 22:39:01 +0530

    new commit

--- hello.ts
+++ hello.ts
@@ -12,6 +12,19 @@ type obj1= {a:string};
 type obj2 = {b:number};
 type inter = obj1 & obj2;
 let egObj1:inter = {a:"21",b:21}
+
+interface abc{
+    name:string
+    getId(): number
+    //or
+    getDbId: ()=> string
+}
+
+let abcDefValue:abc = {name:"abc", getId:() =>{
+    return 2;
+}, getDbId:function a() {
+    return "abc";
+}}
 console.log(3+2);
 console.log(user);
 
